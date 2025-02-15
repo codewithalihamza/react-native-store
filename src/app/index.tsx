305f3f86fa072658ca5a66adb193b45d27e2e242
codewithalihamza <512syedalihamza@gmail.com>
2025-02-15 23:58:09 +0500

    update the title on landing page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,8 +5,8 @@ import "~/global.css";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center gap-4">
-      <Text>Welcome to our react native starter template!</Text>
-      <Button title="Get Started" onPress={() => {}} />
+      <Text>Welcome to our react native project!</Text>
+      <Button title="Get Started" onPress={() => { }} />
     </View>
   );
 }
