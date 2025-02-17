dfdb4c14e442eee8998c1b64f1343c0e95a746b3
codewithalihamza <512syedalihamza@gmail.com>
2025-02-17 23:51:03 +0500

    format the code

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,14 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-    return (
-        <Stack
-            screenOptions={{
-                headerShown: false, // Hide the default header
-            }}
-        >
-            <Stack.Screen name="/login" />
-            <Stack.Screen name="/signup" />
-        </Stack>
-    );
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false, // Hide the default header
+      }}
+    >
+      <Stack.Screen name="/login" />
+      <Stack.Screen name="/signup" />
+    </Stack>
+  );
 }
