69b3317b41677f6c418ac52b0861767e8977b506
codewithalihamza <512syedalihamza@gmail.com>
2025-02-22 22:19:15 +0500

    formatted code

--- src/redux/user-slice.ts
+++ src/redux/user-slice.ts
@@ -1,36 +1,42 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UserState {
-    user: {
-        email: string;
-        password: string;
-        loggedIn: boolean;
-    } | null;
+  user: {
+    email: string;
+    password: string;
+    loggedIn: boolean;
+  } | null;
 }
 
 const initialState: UserState = {
-    user: null,
+  user: null,
 };
 
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        signUpUser: (state, action: PayloadAction<{ email: string; password: string }>) => {
-            state.user = { ...action.payload, loggedIn: false };
-        },
-        loginUser: (state, action: PayloadAction<{ email: string; password: string }>) => {
-            if (
-                state.user &&
-                state.user.email === action.payload.email &&
-                state.user.password === action.payload.password
-            ) {
-                state.user.loggedIn = true;
-            } else {
-                alert("Invalid credentials");
-            }
-        },
+  name: "user",
+  initialState,
+  reducers: {
+    signUpUser: (
+      state,
+      action: PayloadAction<{ email: string; password: string }>,
+    ) => {
+      state.user = { ...action.payload, loggedIn: false };
     },
+    loginUser: (
+      state,
+      action: PayloadAction<{ email: string; password: string }>,
+    ) => {
+      if (
+        state.user &&
+        state.user.email === action.payload.email &&
+        state.user.password === action.payload.password
+      ) {
+        state.user.loggedIn = true;
+      } else {
+        alert("Invalid credentials");
+      }
+    },
+  },
 });
 
 export const { signUpUser, loginUser } = userSlice.actions;
